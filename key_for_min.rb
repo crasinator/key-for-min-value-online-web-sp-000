753e1f3c85711bcc4d25fd8c31edc64335702b86
Jordan Reed <jordanreed03@gmail.com>
2020-12-17 18:43:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def min_key_value_pair (test)
+def min_key_value_pair(test)
+var = nil 
 
 end
