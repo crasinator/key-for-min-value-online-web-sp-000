e112928627c1ee1c42b9044ff7950acb9490b1f7
Jordan Reed <jordanreed03@gmail.com>
2020-12-17 18:42:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def min_key_value_pair (test)
-  if test == {}
-       return nil
-  else
-       test = test.sort_by {|k, v| v}
-       test[0]
-   end
+
 end
