42897f4110e81e05513dc903051c9e381b9d5cef
Jordan Reed <jordanreed03@gmail.com>
2020-12-17 18:41:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(hash)
-
+def min_key_value_pair (test)
+  if test == {}
+       return nil
+  else
+       test = test.sort_by {|k, v| v}
+       test[0]
+   end
 end
